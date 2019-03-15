7190dd80d153688ebd386535cc796e0c36ab784b
Franklin <makyfj@hotmail.com>
2019-03-15 14:04:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,9 +15,6 @@ function yourAnimal() {
 
 function add2(n) {
 
-  const two = Math.floor(Math.random() * 1000);
-  
-  return n += two;
   
   // Feel free to move things around!
 
@@ -25,12 +22,7 @@ function add2(n) {
 }
 
 function funkyFunction() {
-  return function theFunk() {
-    return "FUNKY!"
-  }
 }
-
-var theFunk = funkyFunction();
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
